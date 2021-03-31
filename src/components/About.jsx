73499dfb5b09ee6dee3d0c086a4dbd7e03444a91
jakeFromm <jacobfromm@gmail.com>
2021-03-31 17:45:21 -0400

    fixed articles nav

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ export default function About(){
                 <h3>As a former editor at Time Out New York, and as a contributor to DAZED, Vulture, EW, The InQueery and other sites, he has covered queer, alternative and emerging artistic scenes.</h3> 
                 
                 <h3>On his podcast The Luminaries and at live salons and cabaret shows, he explores the origin stories and cosmic makeup of his guests (and uncovers his own epic saga). Through Tarot and astrology, he offers an archetypal framework for the current moment. He lives, eternally, in New York City. </h3>
-            <Link to="/podcast" style={linkStyles}>
+            <Link to="/articles" style={linkStyles}>
               <h5 id="long-nav" className="inner-nav">EXPLORE DAVID'S WRITING{'>'} </h5>
             </Link>
             </div>

8720c4367b6740cb6c0cc013eb94a803aa676764
jakeFromm <jacobfromm@gmail.com>
2021-04-01 13:26:49 -0400

    redeploying

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -20,13 +20,13 @@ export default {
                 {
                   buildHookId: '603583f6f656350f16e4356f',
                   title: 'Sanity Studio',
-                  name: 'odyssey-sanity-gatsby-netlify-v-8-studio',
+                  name: 'david-odyssey-studio',
                   apiId: '18646566-7b97-4c7c-9b36-3b1e2ebb0e3a'
                 },
                 {
                   buildHookId: '603583f6ba11910b166bd13e',
-                  title: 'Portfolio Website',
-                  name: 'odyssey-sanity-gatsby-netlify-v-8',
+                  title: 'David Odyssey',
+                  name: 'David Odyssey',
                   apiId: 'dc1e2323-005b-4e00-858a-40df9a34951e'
                 }
               ]
@@ -36,12 +36,12 @@ export default {
         data: [
           {
             title: 'GitHub repo',
-            value: 'https://github.com/Jacob-Fromm/odyssey-sanity-gatsby-netlify-v8',
+            value: 'https://github.com/Jacob-Fromm/david-odyssey-burger',
             category: 'Code'
           },
           {
             title: 'Frontend',
-            value: 'https://odyssey-sanity-gatsby-netlify-v-8.netlify.app',
+            value: 'https://davidodyssey.com',
             category: 'apps'
           }
         ]

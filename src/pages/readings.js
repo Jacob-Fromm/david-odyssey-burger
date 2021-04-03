bb6c3ee1b46f16e51526a5e11b2c1a6de1bbc279
jakeFromm <jacobfromm@gmail.com>
2021-04-03 16:36:55 -0400

    punctuation added

--- src/pages/readings.js
+++ src/pages/readings.js
@@ -11,7 +11,7 @@ export default function Readings(){
            <div className="readings-main">
            <div className="readings-image-and-description">
                 {/* <StaticImage src='../images/DAVID_ASTROLOGY.jpg' alt="photo of David Odsyssey holding a crystal ball" /> */}
-                <h3 id="readings-info">David Odyssey offers archetypal, intuitive readings through astrology and the Tarot. All readings are new-user friendly, and conducted over Zoom. Options, rates and reading packages abound; sliding scale is available.<br></br>Contact <a href="mailto: adavidodyssey@gmail.com">adavidodyssey@gmail.com</a> to book a reading</h3>
+                <h3 id="readings-info">David Odyssey offers archetypal, intuitive readings through astrology and the Tarot. All readings are new-user friendly, and conducted over Zoom. Options, rates and reading packages abound; sliding scale is available.<br></br>Contact <a href="mailto: adavidodyssey@gmail.com">adavidodyssey@gmail.com</a> to book a reading.</h3>
             </div>
         </div>
         </div>

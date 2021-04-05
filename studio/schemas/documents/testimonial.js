0895d2e44b70aaaee95e893b4b445326779b8291
jakeFromm <jacobfromm@gmail.com>
2021-04-05 18:47:02 -0400

    triggering redeploy

---
+++ studio/schemas/documents/testimonial.js
@@ -0,0 +1,72 @@
+export default {
+    name: 'testimonial',
+    title: 'Testimonial',
+    type: 'document',
+    fields: [
+        {
+            name: 'headline',
+            title: 'Headline',
+            type: 'string'
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'headline'
+            }
+        },
+        {
+            name: 'url',
+            title: 'URL',
+            type: 'url'
+        },
+        {
+            name: 'publication',
+            title: 'Publication',
+            type: 'string'
+        },
+        {
+            name: 'publicationDate',
+            title: 'Publication Date',
+            type: 'date'
+        },
+        {
+            name: 'image',
+            title: 'Article Image',
+            type: 'figure'
+        },
+        // {
+        //     name: 'image',
+        //     title: 'Article Image',
+        //     type: 'image',
+        //     hidden: false,
+        //     readOnly: false,
+        //     description: 'article image',
+        //     options: {
+        //         hotspot: true
+        //     },
+        //     fields: [
+        //         {
+        //             name: 'caption',
+        //             type: 'string',
+        //             title: 'Caption',
+        //             options: {
+        //                 isHighlighted: true
+        //             }
+        //         },
+        //         {
+        //             name: 'attribution',
+        //             type: 'string',
+        //             title: 'Attribution',
+        //         }
+        //     ]
+        // },
+        {
+            name: 'author',
+            title: 'Author',
+            type: 'reference',
+            to: { type: 'person' }
+        }
+    ]
+}
